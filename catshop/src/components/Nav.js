4e166da51d6ffb271362be6dfa8d6a3943a6d749
Keshia Collins <keshia90.kc@googlemail.com>
2022-06-15 16:50:27 +0100

    Navbar added..possibly

---
+++ catshop/src/components/Nav.js
@@ -0,0 +1,12 @@
+// import { ShopCart } from './ShopCart';
+// import { Link } from 'react-router-dom';
+
+const Navbar = ({ basketItems, removeItemFromBasket }) => {
+	return (
+		<div className="nav">
+{/* link to the homepage and shopcart component ? set up shop basket component to be able to add or remove itemss */}
+		</div>
+	);
+};
+
+export default Navbar;

99336eef10d2bbf32b9eb18137ef0b6eec20856a
Gwyl <Gwylim.l.richards@gmail.com>
2022-06-16 15:22:11 +0100

    reformatted API to import to child components not parents

--- catshop/src/App.js
+++ catshop/src/App.js
@@ -6,45 +6,62 @@ import { faker } from '@faker-js/faker';
 import CatPicture from './components/kittyCats';
 
 const App = () => {
-    const [cat, setCat] = useState([]);
-    const [error, setError] = useState(null);
+    // const [cat, setCat] = useState([]);
+    // const [error, setError] = useState(null);
     const [show, setShow] = useState(false);
-    useEffect(() => {
+    const [basket, setBasket] = useState([]);
+    const [addedCat, setAddedCat] = useState([]);
+    const [catName, setCatName] = useState("");
+    const [catPrice, setCatPrice] = useState(0);
+
+    const addToBasket = (x) => {
+        x.preventDefault();
+        setBasket([...basket, addedCat]);
+        setAddedCat([]);
+    }
+
+    // useEffect(() => {
   
-        const fetchData = async () => { try{
-        const response = await fetch("https://api.thecatapi.com/v1/images/search" ,{headers: {
-                "x-api-key":"e32dd654-b737-4256-bfc1-14d1e95d7349"
-            }
-        });
-        console.log(response);
-        if(!response.ok){
-        throw new Error(response.statusText);
-        }
-        const data = await response.json();
-        console.log(data);
-        setCat(data);
-        console.log(data);
-        } catch(error) {
-        console.log(error);
-        setError('Could not fetch the data');
-        }
-      };
-     fetchData();
-     // eslint-disable-next-line
-      },[]);
+    //     const fetchData = async () => { try{
+    //     const response = await fetch("https://api.thecatapi.com/v1/images/search" ,{headers: {
+    //             "x-api-key":"e32dd654-b737-4256-bfc1-14d1e95d7349"
+    //         }
+    //     });
+    //     console.log(response);
+    //     if(!response.ok){
+    //     throw new Error(response.statusText);
+    //     }
+    //     const data = await response.json();
+    //     setCat(data);
+    //     } catch(error) {
+    //     console.log(error);
+    //     setError('Could not fetch the data');
+    //     }
+    //   };
+    //  fetchData();
+    //  // eslint-disable-next-line
+    //   },[]);
 
     return(
     <div className="App">
         <div>
-        {error && <p>{error}</p>}
+            <CatPicture />
+            <CatPicture />
+            <CatPicture />
+        {/*error && <p>{error}</p>}
         
         {cat.map((pic)=>( 
         <div key={pic.id}>
-            <CatPicture name={faker.name.firstName()} image={pic.url} price={faker.commerce.price(5001,10000,2,'£')} />
+
+            <CatPicture name={faker.name.firstName()} image={pic.url} price={faker.commerce.price(5001,10000,2,'£')} addedCat={addedCat} setAddedCat={setAddedCat} />
+            <button onClick addToBasket></button> 
             {/* <h1>{faker.name.firstName()}</h1>
             <h2>{faker.commerce.price(5001,10000,2,'£')}</h2>
-            <img src={pic.url} alt="xyz" /> */}
-            </div>))}
+        <img src={pic.url} alt="xyz" /> */}
+            
+            {/*</div>))} */}
+
+            
     </div>
 
       <button onClick={() => setShow(true) }>Basket</button>
--- catshop/src/components/kittyCats.js
+++ catshop/src/components/kittyCats.js
@@ -1,18 +1,61 @@
-const CatPicture = ({
-    image,
-    name,
-    price,
-}) => {
+import { faker } from '@faker-js/faker';
+import { useState, useEffect } from 'react';
+
+const CatPicture = (
+    // image,
+    // name,
+    // price,
+    // setAddedCat,
+) => {
+    const [cat, setCat] = useState([]);
+    const [error, setError] = useState(null);
+    const [catName, setCatName] = useState("");
+    const [catPrice, setCatPrice] = useState("");
+    useEffect(() => {
+  
+        const fetchData = async () => { try{
+        const response = await fetch("https://api.thecatapi.com/v1/images/search" ,{headers: {
+                "x-api-key":"e32dd654-b737-4256-bfc1-14d1e95d7349"
+            }
+        });
+        console.log(response);
+        if(!response.ok){
+        throw new Error(response.statusText);
+        }
+        const data = await response.json();
+        setCat(data);
+        setCatName(faker.name.firstName());
+        setCatPrice(faker.commerce.price(5001,10000,2,'£'));
+        } catch(error) {
+        console.log(error);
+        setError('Could not fetch the data');
+        }
+      };
+     fetchData();
+     // eslint-disable-next-line
+      },[]);
     return (
+        <div>
+        {cat.map((pic)=>( 
+            <div key={pic.id}>
+    
+                
+                {/* <button onClick addToBasket></button> 
+                {/* <h1>{faker.name.firstName()}</h1>
+                <h2>{faker.commerce.price(5001,10000,2,'£')}</h2>
+                <img src={pic.url} alt="xyz" /> 
+                
+                </div>)) */}
         <div className="cats">
-            <img src={image} alt=""/>
+            <img src={pic.url} alt=""/>
             <div className="catInfo">
-                <h2>{name}</h2>
-                <h3>{price}</h3>
-            </div>
+                <h2>{catName}</h2>
+                <h3>{catPrice}</h3></div></div></div>   ))}
+                {/*<button onClick={(setAddedCat([name,price]))}>Add to basket</button>*/}
+                
+            
         </div>
-    );
-};
+);}
 
 export default CatPicture;
 

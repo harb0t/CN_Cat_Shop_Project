ef845f61d2c06b5eec995a1d3ea9befa46825dcc
Gwyl <Gwylim.l.richards@gmail.com>
2022-06-17 14:31:46 +0100

    added sum total to basket

--- catshop/src/App.js
+++ catshop/src/App.js
@@ -7,29 +7,23 @@ import CatPicture from './components/kittyCats';
 const App = () => {
     const [show, setShow] = useState(false);
     const [basket, setBasket] = useState([]);
-    const [addedCat, setAddedCat] = useState([]);
-    // const updateBasket = () => {setAddedCat([y,z]);setBasket([...basket, addedCat]);setAddedCat(["",""]);}
-    // const showTheBasket = (x) => {return(<p>{x}</p>)}
-    // const showBasket = useMemo(() => showTheBasket(basket),[basket]);
-
-
-    useEffect(() => {
-
-    })
+    let basketTotal = 0;
+    for(let i=0;i<basket.length;i++){
+        basketTotal += parseInt(basket[i][1].slice(1));
+    }
 
     return(
     <div className="App">
         <div>
+            <CatPicture  setBasket={setBasket} basket={basket}  />
             <CatPicture  setBasket={setBasket} basket={basket} />
-            <CatPicture  setBasket={setBasket} basket={basket} />
-            <CatPicture  setBasket={setBasket} basket={basket} />
-
-        
-    </div>
+            <CatPicture  setBasket={setBasket} basket={basket}  />
+        </div>
 
       <button onClick={() => setShow(true) }>Basket</button>
       <Modal title="Shopping Basket" onClose={() => setShow(false)} show={show}>
       <p>{basket}</p>
+      <p>{basketTotal}</p>
       </Modal>
 </div>
     )}

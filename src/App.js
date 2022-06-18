815f8d8855df0b3d3354e3a70da1cb22493fd350
Dom Harbot <domharbot@gmail.com>
2022-06-18 19:14:49 +0100

    added feat 1914

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import './App.css';
 import React, { useMemo, useState, useEffect } from 'react';
 import Modal from './components/Modal';
 import CatPicture from './components/kittyCats';
-import Container from './components/Container';
-import Header from './components/Header';
-import Title from './components/Title';
+import cat from './components/images/exampleCat.jpg';
+import image from './components/images/droneBanner.png';
 
 const App = () => {
     const [show, setShow] = useState(false);
@@ -15,23 +14,43 @@ const App = () => {
     }
 
     return(
-
-
     <div className="App">
-
-
-
-
+            <div className='aboutMainContainer'>
+    <div className='droneAndTextContainer'>
+    <div className='dronePictureContainer'>
+        <img className='drone' src={image} alt="drone"/>
+    </div>
+    <div className='communityTextContainer'>
+    <h2>Community. Pawsitivity. Kitties</h2>
+    <p>Here At The Kitten Exchange We Are Here For You And All Your Feline Needs. Whether It’s A Fluffy Kitty<br/>Or Smooth Kitty, A Grumpy Kitty Or Cheery Kitty – We Are Here For You. Whilst Offering Many Other<br/> Services (Kitty TV, Kitty VR, PocketKitti) Our Primary Responsibility Is Delivering Your Chosen Kitty With<br/> The Immediacy We All Know, Love And Expect In These Modern Times. For Even Quicky Delivery Be Sure<br/> To Subscribe As A Prime Kitty Member.</p>
+    </div>
+    </div>
+    <div className='aboutMiddleContainer'>
+    </div>
+    <div className='catAndTextContainer'>
+    <div className='purrfectTextContainer'>
+        <h2>The Purrfect Kitty For You</h2>
+        <p>All Our Kitties Are Vat Grown From Amino Acids Through Bulk Extraction From Protein Hydrolysates <br/>To Ensure You Have The Most Optimised Kitty Possible To Suit Your Every Day Needs. Supercharged By K2<br/>Our Models Brings Exceptional Speed And Boops. Boops Anywhere... At Home And At Work</p>
+    </div>
+    <div className='catPictureContainer'>
+    <img className='meow'  src={cat} alt="catCode"/>
+    </div>
+    </div>
+</div>
         <div>
             <CatPicture  setBasket={setBasket} basket={basket}  />
+</div>
+<div>
             <CatPicture  setBasket={setBasket} basket={basket} />
+</div>
+<div>
             <CatPicture  setBasket={setBasket} basket={basket}  />
         </div>
 
       <button onClick={() => setShow(true) }>Basket</button>
       <Modal title="Shopping Basket" onClose={() => setShow(false)} show={show}>
       <p>{basket}</p>
-      <p>{basketTotal}</p>
+      <p>total price: {basketTotal}</p>
       </Modal>
 </div>
     )}
--- src/components/kittyCats.js
+++ src/components/kittyCats.js
@@ -1,5 +1,7 @@
 import { faker } from '@faker-js/faker';
 import { useState, useEffect } from 'react';
+import './kittyCats.css';
+
 
 const CatPicture = (
     {basket,setBasket}
@@ -35,13 +37,21 @@ const CatPicture = (
         <div>
         {cat.map((pic)=>( 
             <div key={pic.id}>
+<div className = "mainModuleContainer">
 
-        <div className="cats">
-            <img src={pic.url} alt=""/>
-            <div className="catInfo">
-                <h2>{catName}</h2>
-                <h3>{catPrice}</h3>
-                <button onClick={() => setBasket([...basket,[catName,catPrice]])}>Add to basket</button>
+<div>
+    <img className = "catModulePic" src={pic.url}/>
+    </div>
+
+            <div className = "catModuleTextBox">
+        <div>
+            <p className = "catName">{catName}</p>
+        </div>
+        <div>
+            <p>{catPrice}</p>
+        </div>
+
+                <button className="purrchaseKittyButton" onClick={() => setBasket([...basket,[catName,catPrice]])}>Add to basket</button>
                 </div></div></div>))}
                 
             
